28517cf0e62eb9bf94a2e61f9d419eaf9ba0288d
TheresNoTime <sam@theresnotime.co.uk>
2022-05-23 16:12:51 +0100

    Add mespeak

---
+++ public_html/projects/jenkins-state/api/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$json = json_decode(file_get_contents('https://integration.wikimedia.org/ci/view/Beta/job/beta-code-update-eqiad/lastBuild/api/json'), true);
+$timestamp = round($json['timestamp'] / 1000);
+echo time() . PHP_EOL;
+echo $timestamp . PHP_EOL;
+$dt = DateTime::createFromFormat('U', $timestamp);
+echo $dt->format('Y-m-d H:i:s');
+var_dump($dt);
