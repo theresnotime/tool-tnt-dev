56a4aa112c02c171dbd120e295bcec9377c37b1f
TheresNoTime <sam@theresnotime.co.uk>
2022-05-24 10:10:07 +0100

    Unclosed bracket!

--- public_html/projects/tts/index.php
+++ public_html/projects/tts/index.php
@@ -131,7 +131,7 @@ $data = json_decode(file_get_contents(__DIR__ . '/static/data.json'), true);
                 <h3>Known issues</h3>
                 <ul>
                     <li>Some of Azure's outputs are missing, as it didn't recognise some of the IPA unicode</li>
-                    <li>larynx's output needs significant tweaking to make it useful (i.e. some of the outputs below sound <i>nothing</i> like they should, mainly due to the use of IPA it doesn't understand.</li>
+                    <li>larynx's output needs significant tweaking to make it useful (i.e. some of the outputs below sound <i>nothing</i> like they should, mainly due to the use of IPA it doesn't understand.)</li>
                 </ul>
 
                 <div class="table-responsive">
